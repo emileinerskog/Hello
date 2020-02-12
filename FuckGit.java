62a0f8fd71ab28e55dee9e2b6ed3cd21160b5147
Emil Einerskog <emil@einerskog.se>
2020-02-12 14:00:30 +0100

    Explain what changes I did

--- FuckGit.java
+++ FuckGit.java
@@ -4,6 +4,10 @@ public class FuckGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		
+		for(int i=0;i<10;i++) {
+			System.out.println("Fuck " + (i+1));
+		}
 	}
 
 }
